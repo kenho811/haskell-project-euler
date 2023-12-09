bafdee6afdaefe644f8242d39a525300ab7f69c5
kenho <kenho811@gmail.com>
2023-12-09 23:57:28 +0800

    add ProblemTwo.hs

---
+++ src/ProblemTwo.hs
@@ -0,0 +1,17 @@
+module ProblemTwo where
+
+getSolution :: Integer
+getSolution = sum $ fiboSeriesLessThan 4000000
+
+
+
+fibo :: (Eq t, Num t, Num a) => t -> a
+fibo n | n == 0 || n == 1 = 1
+       | n == 2  = 2 
+       | otherwise = fibo(n-1) + fibo(n -2)
+
+
+fiboSeriesLessThan :: Integral a => a -> [a]
+fiboSeriesLessThan num = filter even $ takeWhile (< num) $ map fibo [1..]
+
+
