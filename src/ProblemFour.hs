874537b9d57a8ee4e4a16efc6a1c6d28084dbe42
kenho <kenho811@gmail.com>
2023-12-10 15:23:29 +0800

    update chapter 4

---
+++ src/ProblemFour.hs
@@ -0,0 +1,18 @@
+module ProblemFour where
+
+
+getSolution :: Integer
+getSolution = maximum $ filter isNumPalindrom prod
+
+
+prod :: [Integer]
+prod = [x*y | x <- [999, 998.. 1], y <- [999,998..1]]
+
+
+isNumPalindrom :: Integer -> Bool
+isNumPalindrom num =
+  show num == (reverse . show) num
+
+
+
+
