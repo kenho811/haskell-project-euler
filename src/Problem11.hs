dd47a60692fe2cdf8d205ef2191a21739cb9bb53
kenho <kenho811@gmail.com>
2023-12-17 01:03:23 +0800

    complete Problem11

--- src/Problem11.hs
+++ src/Problem11.hs
@@ -7,12 +7,19 @@ import Data.Maybe (catMaybes)
 
 type Coordinate = (Int,Int)
 
+-- 89, 94, 97, 87
+-- (12, 6)
+
 
 runProblem11 :: IO()
 runProblem11 = do
     content <- getInputFromFile
     let dataMap = getNumsFromString content
-    print $ Map.lookup (0, 0) dataMap
+    let coordinates = [(x,y) | x <- [0..19], y <- [0..19]]
+    -- print coordinates
+    -- print $ map (\ (x,y) -> ((x,y), maxAtCoordinate dataMap (x,y))) coordinates
+    print $ maximum $ map (\ (x,y) -> maxAtCoordinate dataMap (x,y)) coordinates
+    -- print $ maxAtCoordinate dataMap (12,6)
 
 
 -- read and parse input
@@ -31,17 +38,24 @@ mayBeProduct xs | length xs /= length (catMaybes xs) = Nothing
                 | otherwise = (Just . product . catMaybes) xs
 
     
-
 maxAtCoordinate :: Map.Map Coordinate Int -> Coordinate -> Maybe Int
 maxAtCoordinate hashMap (rolNum, colNum) =  
-    let rightHorizontal = filter filterCoordinate  $ zip (replicate 3 rolNum) (map (colNum+) [0..3])
-        leftHorizontal = filter filterCoordinate  $ zip (replicate 3 rolNum) (map (colNum-) [0..3])
+    let right = filter filterCoordinate  $ zip (replicate 4 rolNum) (map (colNum+) [0..3])
+        left = filter filterCoordinate  $ zip (replicate 4 rolNum) (map (colNum-) [0..3])
+        up = filter filterCoordinate  $ zip (map (rolNum-) [0..3]) (replicate 4 colNum)
+        down = filter filterCoordinate  $ zip (map (rolNum+) [0..3]) (replicate 4 colNum)
+        rightUp = filter filterCoordinate  $ zip (map (rolNum-) [0..3]) (map (colNum+) [0..3])
         rightDown = filter filterCoordinate  $ zip (map (rolNum+) [0..3]) (map (colNum+) [0..3])
         leftUp = filter filterCoordinate  $ zip (map (rolNum-) [0..3]) (map (colNum-) [0..3])
+        leftDown = filter filterCoordinate  $ zip (map (rolNum+) [0..3]) (map (colNum-) [0..3])
         -- all the values
-        rightHorizontalProduct = mayBeProduct $ map (\(x,y) -> Map.lookup (x, y) hashMap) rightHorizontal
-        leftHorizontalProduct = mayBeProduct $ map (\(x,y) -> Map.lookup (x, y) hashMap) leftHorizontal
+        rightProduct = mayBeProduct $ map (\(x,y) -> Map.lookup (x, y) hashMap) right
+        leftProduct = mayBeProduct $ map (\(x,y) -> Map.lookup (x, y) hashMap) left
+        upProduct = mayBeProduct $ map (\(x,y) -> Map.lookup (x, y) hashMap) up
+        downProduct = mayBeProduct $ map (\(x,y) -> Map.lookup (x, y) hashMap) down
+        rightUpProduct = mayBeProduct $ map (\(x,y) -> Map.lookup (x, y) hashMap) rightUp
         rightDownProduct = mayBeProduct $ map (\(x,y) -> Map.lookup (x, y) hashMap) rightDown
         leftUpProduct = mayBeProduct $ map (\(x,y) -> Map.lookup (x, y) hashMap) leftUp
+        leftDownProduct = mayBeProduct $ map (\(x,y) -> Map.lookup (x, y) hashMap) leftDown
     in
-        maximum [rightHorizontalProduct, leftHorizontalProduct, rightDownProduct, leftUpProduct]
+       maximum [rightProduct, leftProduct, upProduct, downProduct, rightUpProduct, rightDownProduct, leftUpProduct, leftDownProduct]
